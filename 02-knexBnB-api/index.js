b8997fca6e0dfc5532d800958393740aaf011f69
César Guerra <warderer@gmail.com>
2025-04-08 19:07:43 -0600

    ✨ Add basic express server

---
+++ 02-knexBnB-api/index.js
@@ -0,0 +1,16 @@
+// #1 Llamar al framework de express
+const express = require('express')
+
+// #2a Crear una instancia de express
+const app = express()
+const port = process.env.PORT || 3000
+
+// #2b Configurar express para que pueda recibir datos en formato JSON
+app.use(express.json())
+
+// #3 Definir rutas
+
+// #4 Levantar el servidor
+app.listen(port, () => {
+  console.log(`App listening on port ${port} 🚀`)
+})
